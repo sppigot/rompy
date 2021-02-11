596847a2e89dca95820c0c3b6fb59b43ad20e8d2
Paul Branson <paul.branson@csiro.au>
2021-02-11 19:31:06 +0800

    Improve tests

--- rompy/tests/test_intake.py
+++ rompy/tests/test_intake.py
@@ -13,25 +13,25 @@ def test_catalog():
     assert isinstance(intake.cat.rompy_data,Catalog)
 
 
-def test_intake_local():
+def test_intake_local_stack():
     import rompy
     import intake
     import os
-    from intake.catalog import Catalog
-    from intake.catalog.local import LocalCatalogEntry
-    import pandas as pd
 
-    mycat = Catalog.from_dict({
-    'test': LocalCatalogEntry('test', 'test fc stack', 'netcdf_fcstack', 
-                            args={'urlpath': os.path.dirname(os.path.abspath(__file__)) + '/data/{dt}.000000/',
-                            'fn_fmt': 'tab_out.nc',
-                            'fmt_fields': {'dt':list(pd.date_range('2021-02-10','2021-02-12').strftime("%Y%m%d"))},
-                            'ds_filters': {'subset':['hs']},
-                            'hindcast':False}),
-    })
-    ds = mycat.test.to_dask()
+    mycat = intake.open_catalog(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data','catalog.yaml'))
+    ds = mycat.local_stack.to_dask()
+
     assert ds.time.shape == (2,121)
 
+def test_intake_local_hindcast():
+    import rompy
+    import intake
+    import os
+
+    mycat = intake.open_catalog(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data','catalog.yaml'))
+    ds = mycat.local_hindcast.to_dask()
+
+    assert ds.time.shape == (145,)
 
 def test_intake_remote_stack():
     import rompy
@@ -55,7 +55,7 @@ def test_intake_remote_hindcast():
     assert ds.time.shape == (193,)
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
     # test_intake_remote_stack()
     # test_intake_remote_hindcast()
-    # test_intake_local()
+    test_intake_local()
