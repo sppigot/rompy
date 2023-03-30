8f1d80dcc9ace1e6243a45337076d76dad736337
Tom Durrant <t.durrant@oceanum.science>
2023-03-31 10:11:26 +1100

    Fixed tests when not run in tests directory

--- tests/test_swanmodel.py
+++ tests/test_swanmodel.py
@@ -1,20 +1,27 @@
+import os
+
 import pytest
 from utils import compare_files
 
 from rompy.swan import SwanModel
+
+here = os.path.dirname(os.path.abspath(__file__))
 
 
 @pytest.fixture
 def model():
     return SwanModel(
         run_id="test_swan",
-        output_dir="simulations",
+        output_dir=os.path.join(here, "simulations"),
     )
 
 
 def test_generate(model):
     model.generate()
-    compare_files("simulations/test_swan/INPUT", "simulations/test_swan_ref/INPUT")
+    compare_files(
+        os.path.join(here, "simulations/test_base/INPUT"),
+        os.path.join(here, "simulations/test_base_ref/INPUT"),
+    )
 
 
 def test_swan_input():
