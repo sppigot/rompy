a5b45525136cd2b57e42813674371dc0e5a6cee4
Rafael Guedes <r.guedes@oceanum.science>
2023-06-01 17:24:49 +1200

    Remove some extra bits and blackify boundary module

--- rompy/swan/components/boundary.py
+++ rompy/swan/components/boundary.py
@@ -1,41 +1,26 @@
 """Boundary for SWAN."""
-import yaml
-from enum import Enum
 from typing import Literal, Optional, Any
-from enum import Enum
 from pathlib import Path
 from typing_extensions import Literal
-from abc import ABC, abstractmethod
 from pydantic import root_validator, confloat
 
-from rompy.swan.components.base import BaseComponent#, NONSTATIONARY, READINP, GridOptions
-from rompy.swan.subcomponents.boundary import SIDE, SEGMENTXY, SEGMENTIJ, CONSTANTPAR, VARIABLEPAR, CONSTANTFILE, VARIABLEFILE
+from rompy.swan.components.base import BaseComponent
 from rompy.swan.subcomponents.shape import SHAPESPEC
+from rompy.swan.subcomponents.boundary import (
+    SIDE,
+    SEGMENTXY,
+    SEGMENTIJ,
+    CONSTANTPAR,
+    VARIABLEPAR,
+    CONSTANTFILE,
+    VARIABLEFILE,
+)
 
 
 HERE = Path(__file__).parent
 
 
-class BOUNDARY(BaseComponent, ABC):
-    """SWAN boundary.
-
-    This is the base class for boundary. It is not meant to be used directly.
-
-    Parameters
-    ----------
-    model_type: Literal["boundary"]
-        Model type discriminator.
-
-    """
-
-    model_type: Literal["boundary"] = "boundary"
-
-    @abstractmethod
-    def __repr__(self):
-        raise RuntimeError("BOUNDARY is an abstract class and cannot be rendered.")
-
-
-class BOUNDSPEC(BOUNDARY):
+class BOUNDSPEC(BaseComponent):
     """SWAN BOUNDSPEC boundary component.
 
     `BOUNDSPEC SIDE|SEGMENT ... CONSTANT|VARIABLE PAR|FILE ...`
@@ -62,6 +47,7 @@ class BOUNDSPEC(BOUNDARY):
     TODO: Add support for unstructured grid (k).
 
     """
+
     model_type: Literal["boundspec"] = "boundspec"
     shapespec: SHAPESPEC = SHAPESPEC()
     location: SIDE | SEGMENTXY | SEGMENTIJ
@@ -71,12 +57,3 @@ class BOUNDSPEC(BOUNDARY):
         repr = f"{self.shapespec.render()}\n"
         repr += f"BOUNDSPEC {self.location.render()}{self.data.render()}"
         return repr
-
-
-from pydantic import BaseModel
-
-
-
-if __name__ == "__main__":
-    bnd = SIDE(side="west")
-    print(bnd.render())
