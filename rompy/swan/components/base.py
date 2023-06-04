4cd57d43c0dbb794d0fcc6d1babb98141be66dfc
Rafael Guedes <r.guedes@oceanum.science>
2023-06-04 20:37:56 +1200

    Implement cmd splitting so it does not exceed maximum length for line

--- rompy/swan/components/base.py
+++ rompy/swan/components/base.py
@@ -3,7 +3,8 @@
 How to subclass
 ---------------
 
-- Define a new `type` Literal type for the subclass
+- Define a new `model_type` Literal for the subclass (set the default value to avoid
+  having to set it when instantiating the object)
 - Overwrite the __repr__ method to return the SWAN input file string
 
 """
@@ -14,6 +15,37 @@ from rompy.core import RompyBaseModel
 
 
 logger = logging.getLogger(__name__)
+
+MAX_LENGTH = 132
+
+
+def split_string(cmd: str, max_length: int = MAX_LENGTH, spaces: int = 4) -> list:
+    """Split command cmd if longer than max_length.
+
+    Parameters
+    ----------
+    cmd: str
+        SWAN CMD string to split.
+    max_length: int
+        Maximum length of each split string.
+    spaces: int
+        Number of spaces in each identation before each new line.
+
+    Returns
+    -------
+    list
+        Split string.
+
+    """
+    if len(cmd) <= max_length:
+        return [cmd]
+
+    split_index = cmd.rfind(" ", 0, max_length - 1 - spaces)
+
+    if split_index == -1:
+        split_index = max_length
+
+    return [cmd[:split_index]] + split_string(cmd[split_index+1:])
 
 
 class BaseComponent(RompyBaseModel):
@@ -37,6 +69,14 @@ class BaseComponent(RompyBaseModel):
         """Configure the model."""
         extra = "forbid"
 
-    def render(self):
-        """Render the component to a string."""
-        return f"{self.__repr__()}"
+    def render(self) -> str:
+        """Render the component to a CMD string."""
+        spaces = 4
+        # Split cmd at existing newlines
+        cmd_lines = self.__repr__().split("\n")
+        # Split each line before max_length
+        cmds = []
+        for cmd in cmd_lines:
+            cmds.extend(split_string(cmd, max_length=MAX_LENGTH, spaces=spaces))
+        # Return joined string with newline representations
+        return f" &\n{spaces * ' '}".join(cmds)
--- tests/swan/components/test_base.py
+++ tests/swan/components/test_base.py
@@ -1,1 +1,31 @@
 """Test base component."""
+from string import ascii_lowercase, ascii_uppercase
+from typing import Literal
+
+from rompy.swan.components.base import BaseComponent
+
+
+class LongRender(BaseComponent):
+    """Long render method."""
+    model_type: Literal["test"] = "test"
+
+    @property
+    def options(self):
+        return {c: f"{c}_value" for c in ascii_lowercase + ascii_uppercase}
+
+    def __repr__(self):
+        """Render the component to a string."""
+        repr = f"COMPONENT"
+        for k, v in self.options.items():
+            if k == "A":
+                repr += f"\n{k}={v}"
+            else:
+                repr += f" {k}={v}"
+        return repr
+
+
+def test_max_132_characters():
+    """Test that the render string is not longer than 132 characters."""
+    lr = LongRender()
+    print(lr.render())
+    # assert len(long_render.render()) <= 132
