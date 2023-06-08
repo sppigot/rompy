74a25fef0ec41b6c59192c23d51d06d699648b80
Rafael Guedes <r.guedes@oceanum.science>
2023-06-08 22:01:09 +1200

    Add physics component and subcomponent modules and tests, ongoing work

---
+++ rompy/swan/components/physics.py
@@ -0,0 +1,154 @@
+"""Model physics components."""
+import logging
+from typing import Literal, Optional
+from abc import ABC
+from pydantic import validator, root_validator, constr, confloat, conint, Field
+
+from rompy.swan.components.base import BaseComponent
+from rompy.swan.subcomponents.startup import CARTESIAN, SPHERICAL
+
+logger = logging.getLogger(__name__)
+
+
+"""
+[cf10]: controls the linear wave growth.
+Default: [cf10] = 188.
+[cf20] controls the exponential wave growth.
+Default: [cf20] = 0.59
+[cf30] controls the exponential wave growth.
+Default: [cf30] = 0.12
+[cf40] controls the dissipation rate, i.e., the time decay scale.
+Default: [cf40] = 250.
+[edmlpm] maximum non-dimensionless energy density of the wind sea part of the spectrum
+according to Pierson Moskowitz.
+Default: [edmlpm] = 0.0036
+[cdrag] drag coefficient.
+Default: [cdrag] = 0.0012
+[umin] minimum wind velocity (relative to current; all wind speeds are taken at 10 m above sea level). Default: [umin] = 1.
+[cfpm] coefficient which determines the Pierson Moskowitz frequency: dP M = 2πg [cfpm] /U10 Default: [cfpm] = 0.13
+"""
+
+
+class GEN1(BaseComponent):
+    """Physics component GEN1.
+
+    `GEN1 [cf10] [cf20] [cf30] [cf40] [edmlpm] [cdrag] [umin] [cfpm]`
+
+    With this command the user indicates that SWAN should run in first-generation mode
+    (see Scientific/Technical documentation).
+
+    """
+
+    model_type: Literal["gen1"] = Field(
+        default="gen1", description="Model type discriminator"
+    )
+    cf10: Optional[float] = Field(
+        description="Controls the linear wave growth (SWAN default: 188.0)"
+    )
+    cf20: Optional[float] = Field(
+        description="Controls the exponential wave growth (SWAN default: 0.59)"
+    )
+    cf30: Optional[float] = Field(
+        description="Controls the exponential wave growth (SWAN default: 0.12)"
+    )
+    cf40: Optional[float] = Field(
+        description="Controls the dissipation rate, i.e., the time decay scale (SWAN default: 250.0)"
+    )
+    edmlpm: Optional[float] = Field(
+        description="Maximum non-dimensionless energy density of the wind sea part of the spectrum according to Pierson Moskowitz (SWAN default: 0.0036)"
+    )
+    cdrag: Optional[float] = Field(
+        description="Drag coefficient (SWAN default: 0.0012)"
+    )
+    umin: Optional[float] = Field(
+        description="Minimum wind velocity (relative to current; all wind speeds are taken at 10 m above sea level) (SWAN default: 1)"
+    )
+    cfpm: Optional[float] = Field(
+        description="Coefficient which determines the Pierson Moskowitz frequency: $delta_{PM} = 2pi g / U_{10}$ (SWAN default: 0.13)"
+    )
+
+    def cmd(self):
+        repr = "GEN1"
+        if self.cf10 is not None:
+            repr += f" cf10={self.cf10}"
+        if self.cf20 is not None:
+            repr += f" cf20={self.cf20}"
+        if self.cf30 is not None:
+            repr += f" cf30={self.cf30}"
+        if self.cf40 is not None:
+            repr += f" cf40={self.cf40}"
+        if self.edmlpm is not None:
+            repr += f" edmlpm={self.edmlpm}"
+        if self.cdrag is not None:
+            repr += f" cdrag={self.cdrag}"
+        if self.umin is not None:
+            repr += f" umin={self.umin}"
+        if self.cfpm is not None:
+            repr += f" cfpm={self.cfpm}"
+        return repr
+
+
+class GEN2(GEN1):
+    """Physics component GEN2.
+
+    `GEN2 [cf10] [cf20] [cf30] [cf40] [cf50] [cf60] [edmlpm] [cdrag] [umin] [cfpm]`
+
+    With this command the user indicates that SWAN should run in second-generation mode
+    (see Scientific/Technical documentation).
+
+    """
+    model_type: Literal["gen2"] = "gen2"
+    cf50: Optional[float] = Field(
+        description="Controls the spectral energy scale of the limit spectrum (SWAN default: 0.0023)"
+    )
+    cf60: Optional[float] = Field(
+        description="Ccontrols the spectral energy scale of the limit spectrum (SWAN default: -0.223"
+    )
+
+    def cmd(self):
+        repr = "GEN2"
+        if self.cf10 is not None:
+            repr += f" cf10={self.cf10}"
+        if self.cf20 is not None:
+            repr += f" cf20={self.cf20}"
+        if self.cf30 is not None:
+            repr += f" cf30={self.cf30}"
+        if self.cf40 is not None:
+            repr += f" cf40={self.cf40}"
+        if self.cf50 is not None:
+            repr += f" cf50={self.cf50}"
+        if self.cf60 is not None:
+            repr += f" cf60={self.cf60}"
+        if self.edmlpm is not None:
+            repr += f" edmlpm={self.edmlpm}"
+        if self.cdrag is not None:
+            repr += f" cdrag={self.cdrag}"
+        if self.umin is not None:
+            repr += f" umin={self.umin}"
+        if self.cfpm is not None:
+            repr += f" cfpm={self.cfpm}"
+        return repr
+
+
+class GEN3(BaseComponent):
+    """Physics component GEN3.
+
+    `GEN3 JANSSEN|KOMEN|WESTHUYSEN|ST6 (...) AGROW [a]`
+
+    With this command the user indicates that SWAN should run in third-generation mode
+    for wind input, quadruplet interactions and whitecapping. Triads, bottom friction
+    and depth-induced breaking are not activated by this command. See also the
+    Scientific/Technical documentation for more information. The option GEN3 WESTH is
+    default. Note that SWAN converts U10 to U* using a wind drag formula to be
+    specified below. The choices with respect to option GEN3 ST6 are elaborated on
+    here. Some background information on these choices can be found in
+    Rogers et al. (2012) (henceforth denoted as RBW12).
+
+    """
+    model_type: Literal["gen3"] = "gen3"
+
+    # @root_validator
+    # def no_wind_drag_if_st6(cls, values):
+    #     """ Ensure WU is not prescribed with ST6."""
+        # return values
+
