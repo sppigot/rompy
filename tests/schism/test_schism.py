71877df89b0dd86f10684cbe3894ae81bba5eb29
Tom Durrant <t.durrant@oceanum.science>
2023-09-14 22:54:17 +1000

    Added basic schism render test

--- tests/schism/test_schism.py
+++ tests/schism/test_schism.py
@@ -1,10 +1,16 @@
+import sys
 from datetime import datetime
 from pathlib import Path
+
+from utils import compare_files
 
 from rompy.core import TimeRange
 from rompy.model import ModelRun
 from rompy.schism import SchismConfig
 from rompy.schism.config import DEFAULT_TEMPLATE
+
+sys.path.append("../")
+
 
 # from ..utils import compare_files
 
@@ -13,21 +19,18 @@ here = Path(__file__).parent
 
 def test_schism_render(tmpdir):
     """Test the swantemplate function."""
+    run_id = "test_schism"
     time = TimeRange(
         start=datetime(2020, 2, 21, 4), end=datetime(2020, 2, 24, 4), interval="15M"
     )
     runtime = ModelRun(
-        run_id="test_schism",
+        run_id=run_id,
         output_dir=str(tmpdir),
         config=SchismConfig(),
     )
     runtime.generate()
-    # compare_files(
-    #     here / "simulations" / "test_swan_ref" / "INPUT_NEW",
-    #     tmpdir / runtime.run_id / "INPUT",
-    # )
-
-
-# def test_schism_config(tmpdir):
-#     """Test the swantemplate function."""
-#     config = SchismConfig()
+    for fname in ["param.nml", "wwminput.nml"]:
+        compare_files(
+            here / "reference_files" / runtime.run_id / fname,
+            tmpdir / runtime.run_id / fname,
+        )
