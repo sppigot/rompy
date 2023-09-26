f49c9d4788223b8e8a29dad19f3deba476d82be2
Rafael Guedes <r.guedes@oceanum.science>
2023-09-26 16:53:44 +1300

    Removed unused imports

--- rompy/swan/subcomponents/numerics.py
+++ rompy/swan/subcomponents/numerics.py
@@ -1,9 +1,6 @@
 """SWAN numerics subcomponents."""
-from typing import Annotated, Literal, Optional, Union
-from pydantic import field_validator, Field, model_validator
-from abc import ABC
-from pydantic_numpy.typing import Np2DArray
-import numpy as np
+from typing import Literal, Optional, Union
+from pydantic import Field
 
 from rompy.swan.subcomponents.base import BaseSubComponent
 
--- rompy/swan/subcomponents/physics.py
+++ rompy/swan/subcomponents/physics.py
@@ -3,7 +3,6 @@ from typing import Annotated, Literal, Optional
 from pydantic import field_validator, Field, model_validator
 from abc import ABC
 from pydantic_numpy.typing import Np2DArray
-import numpy as np
 
 from rompy.swan.subcomponents.base import BaseSubComponent
 
