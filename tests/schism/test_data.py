9602938520e952502ec4379222837d65705fed0e
Tom Durrant <t.durrant@oceanum.science>
2023-12-15 17:10:28 +1100

    Added ocean boundary tests

--- tests/schism/test_data.py
+++ tests/schism/test_data.py
@@ -7,9 +7,9 @@ import pytest
 import xarray as xr
 
 from rompy.core import BaseGrid, DataBlob, DataGrid, TimeRange
-from rompy.core.data import (SourceDatamesh, SourceDataset, SourceFile,
-                             SourceIntake)
-from rompy.schism.data import SCHISMDataSflux, SfluxAir
+from rompy.core.data import SourceDatamesh, SourceDataset, SourceFile, SourceIntake
+from rompy.schism import SCHISMGrid2D, SCHISMGrid3D
+from rompy.schism.data import SCHISMDataBoundary, SCHISMDataSflux, SfluxAir
 from rompy.schism.namelists import Sflux_Inputs
 
 HERE = Path(__file__).parent
@@ -17,18 +17,23 @@ DATAMESH_TOKEN = os.environ.get("DATAMESH_TOKEN")
 
 
 @pytest.fixture
-def grid_data_source():
+def grid2d():
+    return SCHISMGrid2D(hgrid=DataBlob(source="test_data/hgrid.gr3"))
+
+
+@pytest.fixture
+def grid_atmos_source():
     return SourceIntake(
         dataset_id="era5",
         catalog_uri=HERE / ".." / "data" / "catalog.yaml",
     )
 
 
-def test_atmos(tmp_path, grid_data_source):
+def test_atmos(tmp_path, grid_atmos_source):
     data = SCHISMDataSflux(
         air_1=SfluxAir(
             id="air_1",
-            source=grid_data_source,
+            source=grid_atmos_source,
             filter={
                 "sort": {"coords": ["latitude"]},
                 "crop": {
@@ -40,3 +45,17 @@ def test_atmos(tmp_path, grid_data_source):
         )
     )
     data.get(tmp_path)
+
+
+def test_oceansource(tmp_path, grid=grid2d):
+    data = SCHISMDataBoundary(
+        id="hycom",
+        source=SourceFile(
+            id="ocean_1",
+            uri=HERE / ".." / "data" / "hycom.nc",
+        ),
+        variable="surf_el",
+        coords={"t": "time", "y": "ylat", "x": "xlon", "z": "depth"},
+    )
+    grid = SCHISMGrid2D(hgrid=DataBlob(source="test_data/hgrid.gr3"))
+    data.get(tmp_path, grid)
