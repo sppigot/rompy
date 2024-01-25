9025218cc9806b054069715a3d522a3d8f1dfc2d
Tom Durrant <t.durrant@oceanum.science>
2024-01-25 11:31:58 +1100

    Changed tidal data source to avoid nans

--- tests/schism/test_schism_data.py
+++ tests/schism/test_schism_data.py
@@ -89,10 +89,16 @@ def test_oceandata(tmp_path, grid2d, hycom_bnd):
 
 
 def test_tidal_boundary(tmp_path, grid2d):
+    if not (HERE / "test_data" / "tpxo9-neaus" / "h_m2s2n2.nc").exists():
+        from utils import untar_file
+
+        untar_file(HERE / "test_data" / "tpxo9-neaus.tar.gz", HERE / "test_data/")
+    from utils import untar_file
+
     tides = SCHISMDataTides(
         tidal_data=TidalDataset(
-            elevations=HERE / "test_data" / "tpxo9-test" / "h_m2s2n2.nc",
-            velocities=HERE / "test_data" / "tpxo9-test" / "u_m2s2n2.nc",
+            elevations=HERE / "test_data" / "tpxo9-neaus" / "h_m2s2n2.nc",
+            velocities=HERE / "test_data" / "tpxo9-neaus" / "u_m2s2n2.nc",
         ),
         constituents=["M2", "S2", "N2"],
     )
